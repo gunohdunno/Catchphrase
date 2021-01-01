0cc79eebc406f2a9e6dfc73cfe2633edb8f18450
gunohdunno <kgjung253@gmail.com>
2021-01-01 16:50:24 -0600

    Added menu items in action bar

--- app/src/main/java/com/example/catchphrase/MainActivity.kt
+++ app/src/main/java/com/example/catchphrase/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.catchphrase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.appToolBar))
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            // User chose the "Settings" action
+            true
+        }
+        R.id.action_add -> {
+            // User chose the "Add" action
+            true
+        }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
     }
 }
